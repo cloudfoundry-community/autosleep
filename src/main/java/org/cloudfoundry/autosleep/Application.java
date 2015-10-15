dee0f71bd52b755cb1fac9ee862a97d504d29f55
Arnaud Ruffin <arnaud.ruffin@orange.com>
2015-10-15 15:08:05 +0200

    Replaced @SpringBootApplication with ComponentScan + Configuration + EnableAutoConfiguration, to avoid @Autowire issues on intellij

--- src/main/java/org/cloudfoundry/autosleep/Application.java
+++ src/main/java/org/cloudfoundry/autosleep/Application.java
@@ -2,9 +2,13 @@ package org.cloudfoundry.autosleep;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @Slf4j
 public class Application {
     
