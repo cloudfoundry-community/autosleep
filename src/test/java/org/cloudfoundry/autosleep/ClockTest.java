7e9192e4febd602f2dedd5f15babe3dcff9e63a5
Arnaud Ruffin <arnaud.ruffin@orange.com>
2015-10-15 17:18:15 +0200

    Add test for Clock

---
+++ src/test/java/org/cloudfoundry/autosleep/ClockTest.java
@@ -0,0 +1,58 @@
+package org.cloudfoundry.autosleep;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest("server.port:0")//random port
+@Slf4j
+public class ClockTest {
+
+    private static final long PERIOD_IN_MILLIS = 500;
+    private static final String TEST_ID = "93847";
+
+    @Autowired
+    protected Clock clock;
+    long lastLaunchTime;
+    private int count = 0;
+
+    private Runnable runnable = () -> {
+        log.debug("Ticking");
+        count++;
+        lastLaunchTime = System.currentTimeMillis();
+    };
+
+
+    @Test(timeout = 5000)
+    public void testStartTimer() throws Exception {
+        clock.startTimer(TEST_ID, 0, PERIOD_IN_MILLIS, TimeUnit.MILLISECONDS, runnable);
+        while (count < 3) {
+            Thread.sleep(200);
+        }
+        //if we reach this line before the timeout, the test worked
+        assert true;
+    }
+
+
+    @Test
+    public void testStopTimer() throws Exception {
+        clock.startTimer(TEST_ID, 0, PERIOD_IN_MILLIS, TimeUnit.MILLISECONDS, runnable);
+        Thread.sleep(1000);
+        assertTrue(lastLaunchTime >= System.currentTimeMillis() - PERIOD_IN_MILLIS);
+        clock.stopTimer(TEST_ID);
+        Thread.sleep(1000);
+        assertFalse(lastLaunchTime >= System.currentTimeMillis() - PERIOD_IN_MILLIS);
+    }
+}
