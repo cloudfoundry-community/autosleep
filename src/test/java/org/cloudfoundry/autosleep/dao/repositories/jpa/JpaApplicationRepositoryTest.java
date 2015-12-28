3b9400b38fda4d0146fd9a77791a8e81f887617d
Arnaud Ruffin <arnaud.ruffin@orange.com>
2015-12-28 11:32:27 +0100

    add 'integration-test' profile condition to run JPA tests

--- src/test/java/org/cloudfoundry/autosleep/dao/repositories/jpa/JpaApplicationRepositoryTest.java
+++ src/test/java/org/cloudfoundry/autosleep/dao/repositories/jpa/JpaApplicationRepositoryTest.java
@@ -1,15 +1,11 @@
 package org.cloudfoundry.autosleep.dao.repositories.jpa;
 
 import org.cloudfoundry.autosleep.dao.repositories.AppRepositoryTest;
-import org.junit.Assume;
-import org.junit.BeforeClass;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ActiveProfiles;
 
+@IfProfileValue(name = "integration-test", value = "true")
 @ActiveProfiles({"mysql", "mysql-local"})
 public class JpaApplicationRepositoryTest extends AppRepositoryTest {
 
-    @BeforeClass
-    public static void skipIfNoMysql() {
-        Assume.assumeTrue("Mysql should be present to run this test", JpaUtil.isMySqlPresent());
-    }
 }
--- src/test/java/org/cloudfoundry/autosleep/dao/repositories/jpa/JpaSpaceEnrollerConfigRepositoryTest.java
+++ src/test/java/org/cloudfoundry/autosleep/dao/repositories/jpa/JpaSpaceEnrollerConfigRepositoryTest.java
@@ -1,14 +1,11 @@
 package org.cloudfoundry.autosleep.dao.repositories.jpa;
 
 import org.cloudfoundry.autosleep.dao.repositories.ServiceRepositoryTest;
-import org.junit.Assume;
-import org.junit.BeforeClass;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ActiveProfiles;
 
+@IfProfileValue(name = "integration-test", value = "true")
 @ActiveProfiles({"mysql", "mysql-local"})
 public class JpaSpaceEnrollerConfigRepositoryTest extends ServiceRepositoryTest {
-    @BeforeClass
-    public static void skipIfNoMysql() {
-        Assume.assumeTrue("Mysql should be present to run this test", JpaUtil.isMySqlPresent());
-    }
+
 }
