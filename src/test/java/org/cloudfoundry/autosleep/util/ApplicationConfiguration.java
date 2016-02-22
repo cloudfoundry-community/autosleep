7736ab5bdc0f3e50eebcb3d1262c66ee4ec8bc13
Arnaud Ruffin <arnaud.ruffin@orange.com>
2016-02-22 15:32:48 +0100

    add license header

--- src/test/java/org/cloudfoundry/autosleep/util/ApplicationConfiguration.java
+++ src/test/java/org/cloudfoundry/autosleep/util/ApplicationConfiguration.java
@@ -1,3 +1,22 @@
+/**
+ * Autosleep
+ * Copyright (C) 2016 Orange
+ * Authors: Benjamin Einaudi   benjamin.einaudi@orange.com
+ *          Arnaud Ruffin      arnaud.ruffin@orange.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cloudfoundry.autosleep.util;
 
 import org.springframework.context.annotation.Bean;
@@ -5,9 +24,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-/**
- * Created by buce8373 on 18/12/2015.
- */
 @Configuration
 @PropertySource("classpath:application.properties")
 public class ApplicationConfiguration {
