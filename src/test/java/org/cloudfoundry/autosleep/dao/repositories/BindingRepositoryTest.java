c6bd939f8c481bda6c90aba1ef5962beb17fae5a
Ben Einaudi <benjamin.einaudi@orange.com>
2016-03-15 15:54:56 +0100

    #153: Refactor of BindingRepositoryTest

--- src/test/java/org/cloudfoundry/autosleep/dao/repositories/BindingRepositoryTest.java
+++ src/test/java/org/cloudfoundry/autosleep/dao/repositories/BindingRepositoryTest.java
@@ -32,18 +32,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.toIntExact;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -64,64 +65,144 @@ public abstract class BindingRepositoryTest {
         dao.deleteAll();
     }
 
-    @Test
-    public void testInsert() {
-        dao.save(Binding.builder()
-                .serviceBindingId("testInsert")
-                .serviceInstanceId("testInsert")
-                .resourceId(APP_GUID)
-                .resourceType(ResourceType.Application)
-                .build());
-        assertThat(countServices(), is(equalTo(1)));
+    private int countServices() {
+        return toIntExact(dao.count());
     }
 
     @Test
-    public void testMultipleInsertsAndRetrieves() {
-        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
-        String serviceId = "testServiceId";
-        List<Binding> initialList = new ArrayList<>();
-        ids.forEach(id -> initialList.add(Binding.builder()
-                .serviceBindingId(id)
-                .serviceInstanceId(serviceId)
+    public void test_delete_all() {
+        //Given db has some ids
+        List<String> ids = Arrays.asList("deleteId1", "deleteId2", "deleteId3", "deleteId4");
+
+        Binding.BindingBuilder builder = Binding.builder()
                 .resourceId(APP_GUID)
                 .resourceType(ResourceType.Application)
-                .build()));
+                .serviceInstanceId("service");
 
-        //test save all
+        ids.forEach(id -> dao.save(builder.serviceBindingId(id).build()));
+
+        //When all are deleted
+        dao.deleteAll();
+
+        //Then the returned count is null
+        assertThat(countServices(), is(equalTo(0)));
+
+    }
+
+    @Test
+    public void test_delete_by_entity_decrease_count() {
+        //Given db has some ids
+        List<String> ids = Arrays.asList("deleteId1", "deleteId2", "deleteId3", "deleteId4");
+
+        Binding.BindingBuilder builder = Binding.builder()
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application)
+                .serviceInstanceId("service");
+
+        ids.forEach(id -> dao.save(builder.serviceBindingId(id).build()));
+
+        int nbServicesInit = ids.size();
+
+        //When an entity is deleted by entity
+        dao.delete(dao.findOne(ids.get(0)));
+
+        //Then the count is decremented
+        assertThat(countServices(), is(equalTo(nbServicesInit - 1)));
+    }
+
+    @Test
+    public void test_delete_by_id_decrease_count() {
+        //Given db has some ids
+        List<String> ids = Arrays.asList("deleteId1", "deleteId2", "deleteId3", "deleteId4");
+
+        Binding.BindingBuilder builder = Binding.builder()
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application)
+                .serviceInstanceId("service");
+
+        ids.forEach(id -> dao.save(builder.serviceBindingId(id).build()));
+
+        int nbServicesInit = ids.size();
+
+        //When an entity is deleted by id
+        dao.delete(ids.get(0));
+
+        //Then the count is decremented
+        assertThat(countServices(), is(equalTo(nbServicesInit - 1)));
+    }
+
+    @Test
+    public void test_delete_in_mass() {
+        //Given db has some ids
+        List<String> ids = Arrays.asList("deleteId1", "deleteId2", "deleteId3", "deleteId4");
+
+        Binding.BindingBuilder builder = Binding.builder()
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application)
+                .serviceInstanceId("service");
+
+        ids.forEach(id -> dao.save(builder.serviceBindingId(id).build()));
+
+        int nbServicesInit = ids.size();
+
+        //When we request the deletion giving two existing ids
+        Iterable<Binding> services = dao.findAll(Arrays.asList("deleteId1", "deleteId2"));
+        dao.delete(services);
+
+        //Then the count is decremented
+        assertThat(countServices(), is(equalTo(nbServicesInit - 2)));
+    }
+
+    @Test
+    public void test_empty_database_has_count_0() {
+        assertThat(countServices(), is(equalTo(0)));
+    }
+
+    @Test
+    public void test_find_by_resource_id_and_type_on_existing_type() {
+        //Given there is some entity in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
+
         dao.save(initialList);
-        assertThat("Count should be equal to the amount inserted", countServices(), is(equalTo(
-                initialList.size())));
-
-        //test "exist"
-        ids.forEach(id -> assertThat("Each element should exist in DAO", dao.exists(id), is(true)));
-
-        //test that retrieving all elements give the same amount
-        Iterable<Binding> storedElement = dao.findAll();
-        int count = 0;
-        for (Binding object : storedElement) {
-            count++;
-        }
+        //When we count element by resource and type that were inserted
+        int count = dao.findByResourceIdAndType(Arrays.asList(APP_GUID, APP_GUID), ResourceType.Application).size();
+        //then the count is equal to the one inserted
         assertTrue("Retrieving all elements should return the same quantity", count == initialList
                 .size());
+    }
 
-        //test find with all inserted ids
-        storedElement = dao.findAll(ids);
-        for (Binding object : storedElement) {
-            assertTrue("Retrieved element should be the same as initial element", initialList.contains(object));
-        }
+    @Test
+    public void test_find_by_resource_id_and_type_on_non_existing_type() {
+        //Given there is some entity in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
 
-
-        count = dao.findByResourceIdAndType(Arrays.asList(APP_GUID,APP_GUID),ResourceType.Application).size();
-        assertTrue("Retrieving all elements should return the same quantity", count == initialList
-                .size());
-
-        count = dao.findByResourceIdAndType(Collections.singletonList(APP_GUID),ResourceType.Route).size();
+        dao.save(initialList);
+        //When we count element by resource and type that were not inserted
+        int count = dao.findByResourceIdAndType(Collections.singletonList(APP_GUID), ResourceType.Route).size();
+        //then the count is equal to zero
         assertTrue("No route binding should be found", count == 0);
 
     }
 
     @Test
-    public void testEquality() {
+    public void test_find_one_preserve_equality() {
+        //Given an object is inserted
         String bindingId = "bidingIdEquality";
         String serviceId = "serviceIdEquality";
         Binding original = Binding.builder()
@@ -132,58 +213,145 @@ public abstract class BindingRepositoryTest {
                 .build();
 
         dao.save(original);
+
+        //When we re load it
         Binding binding = dao.findOne(bindingId);
+
+        //Then it is found
         assertFalse("Service binding should have been found", binding == null);
+        //And all attrributes are equals to the one inserted
         assertThat(binding.getServiceInstanceId(), is(equalTo(serviceId)));
         assertThat(binding.getResourceId(), is(equalTo(APP_GUID)));
         assertThat(binding, is(equalTo(original)));
-        assertTrue("Succeed in getting a binding that does not exist", dao.findOne("testGetServiceFail") == null);
 
     }
-
 
     @Test
-    public void testCount() {
-        assertThat(countServices(), is(equalTo(0)));
-    }
+    public void test_find_one_returns_null_on_non_existing_id() {
+        //Given an object is inserted
+        String bindingId = "bidingIdEquality";
+        String serviceId = "serviceIdEquality";
+        Binding original = Binding.builder()
+                .serviceBindingId(bindingId)
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application)
+                .build();
 
+        dao.save(original);
+
+        //When we load a non existing id
+        Binding binding = dao.findOne(bindingId + "-fail");
+        //then the object returned is null
+        assertThat("Succeed in getting a binding that does not exist", binding, is(nullValue()));
+    }
 
     @Test
-    public void testDelete() {
-        final String deleteByIdSuccess = "deleteByIdSuccess";
-        final String deleteByInstanceSuccess = "deleteByInstanceSuccess";
-        final String deleteByMass1 = "deleteByMass1";
-        final String deleteByMass2 = "deleteByMass2";
-        Binding.BindingBuilder builder = Binding.builder();
-        builder.resourceId(APP_GUID).resourceType(ResourceType.Application).serviceInstanceId("service");
-        dao.save(builder.serviceBindingId(deleteByIdSuccess).build());
-        dao.save(builder.serviceBindingId(deleteByInstanceSuccess).build());
-        dao.save(builder.serviceBindingId(deleteByMass1).build());
-        dao.save(builder.serviceBindingId(deleteByMass2).build());
+    public void test_findall_filtered() {
+        //Given there is some entity in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
 
-        int nbServicesInit = 4;
-        assertThat(countServices(), is(equalTo(nbServicesInit)));
+        dao.save(initialList);
+        dao.save(bindingBuilder.serviceBindingId("testInsert3").build());
 
-        //delete a service by binding id
-        dao.delete(deleteByIdSuccess);
-        assertThat(countServices(), is(equalTo(nbServicesInit - 1)));
-
-        //delete a service by name
-        dao.delete(dao.findOne(deleteByInstanceSuccess));
-        assertThat(countServices(), is(equalTo(nbServicesInit - 2)));
-
-        //delete multiple services
-        Iterable<Binding> services = dao.findAll(Arrays.asList(deleteByMass1, deleteByMass2));
-        dao.delete(services);
-        assertThat(countServices(), is(equalTo(nbServicesInit - 4)));
-
-        //delete all services
-        dao.deleteAll();
-        assertTrue(countServices() == 0);
+        //when we iterate on filtered objects
+        //then we have objects that existed
+        int count = 0;
+        for (Binding object : dao.findAll(ids)) {
+            assertTrue("Retrieved element should be the same as initial element", initialList.contains(object));
+            count++;
+        }
+        //and  we find the good number of objects
+        assertTrue("Retrieving filtered elements should return the same quantity", count == initialList
+                .size());
 
     }
 
-    private int countServices() {
-        return toIntExact(dao.count());
+    @Test
+    public void test_findall_no_filter() {
+        //Given there is some entity in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
+
+        dao.save(initialList);
+
+        //when we iterate on all objects
+        int count = 0;
+        for (Iterator<Binding> itElements = dao.findAll().iterator(); itElements.hasNext(); itElements.next() ) {
+            count++;
+        }
+        //Then  we find the good number of objects
+        assertTrue("Retrieving all elements should return the same quantity", count == initialList
+                .size());
+
+    }
+
+    @Test
+    public void test_insert_increase_count() {
+        //Given there is no entity in database
+        //When an entity is inserted
+        dao.save(Binding.builder()
+                .serviceBindingId("testInsert")
+                .serviceInstanceId("testInsert")
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application)
+                .build());
+        //Then count is equal to one
+        assertThat(countServices(), is(equalTo(1)));
+    }
+
+    @Test
+    public void test_multiple_insert_increase_count() {
+        //Given there is some entities in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
+
+        dao.save(initialList);
+        //When we request count
+        //Then count is equal to number we inserted
+        assertThat("Count should be equal to the amount inserted", countServices(),
+                is(equalTo(initialList.size())));
+    }
+
+    @Test
+    public void test_retrieve_by_id() {
+        //Given there is some entity in database
+        List<String> ids = Arrays.asList("testInsert1", "testInsert2");
+        String serviceId = "testServiceId";
+        Binding.BindingBuilder bindingBuilder = Binding.builder()
+                .serviceInstanceId(serviceId)
+                .resourceId(APP_GUID)
+                .resourceType(ResourceType.Application);
+        List<Binding> initialList = ids.stream()
+                .map(id -> bindingBuilder.serviceBindingId(id).build())
+                .collect(Collectors.toList());
+
+        dao.save(initialList);
+
+        //when we request each object by id
+        //Then objects are found
+        ids.forEach(id -> assertThat("Each element should exist in DAO", dao.exists(id), is(true)));
     }
 }
