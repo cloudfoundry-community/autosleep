f3df90d44191f2447d89e39fb97a35ae2bded3b4
Ben Einaudi <benjamin.einaudi@orange.com>
2016-03-16 10:20:08 +0100

    Fix 153 : Refactor of ParameterReaderFactoryTest

--- src/test/java/org/cloudfoundry/autosleep/ui/servicebroker/service/parameters/ParameterReaderFactoryTest.java
+++ src/test/java/org/cloudfoundry/autosleep/ui/servicebroker/service/parameters/ParameterReaderFactoryTest.java
@@ -20,6 +20,8 @@
 package org.cloudfoundry.autosleep.ui.servicebroker.service.parameters;
 
 import org.cloudfoundry.autosleep.config.Config;
+import org.cloudfoundry.autosleep.config.Config.ServiceInstanceParameters;
+import org.cloudfoundry.autosleep.config.Config.ServiceInstanceParameters.Enrollment;
 import org.cloudfoundry.autosleep.ui.servicebroker.service.InvalidParameterException;
 import org.junit.Test;
 
@@ -38,102 +40,231 @@ public class ParameterReaderFactoryTest {
     private ParameterReaderFactory factory = new ParameterReaderFactory();
 
     @Test
-    public void testBuildIdleDurationReader() {
-        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
-        assertThat(idleDurationReader.getParameterName(), is(equalTo(Config.ServiceInstanceParameters.IDLE_DURATION)));
-
-        //default case
-        assertThat(idleDurationReader.readParameter(null, true),
-                is(equalTo(Config.DEFAULT_INACTIVITY_PERIOD)));
-        assertThat(idleDurationReader.readParameter(null, false),
-                is(nullValue()));
-
-        //bad syntax
-        verifyThrown(() -> idleDurationReader
-                        .readParameter("12",
-                                true),
-                InvalidParameterException.class,
-                parameterChecked ->
-                        assertThat(parameterChecked.getParameterName(),
-                                is(equalTo(Config.ServiceInstanceParameters.IDLE_DURATION))));
-
-        //good syntax
-        assertThat(idleDurationReader.readParameter("PT12M", true), is(equalTo(Duration.ofMinutes(12))));
-
-    }
-
-
-    @Test
-    public void testBuildAutoEnrollmentReader() {
-        ParameterReader<Config.ServiceInstanceParameters.Enrollment> enrollmentParameterReader = factory
-                .buildAutoEnrollmentReader();
-        assertThat(enrollmentParameterReader.getParameterName(),
-                is(equalTo(Config.ServiceInstanceParameters.AUTO_ENROLLMENT)));
-        //default case are null
-        assertThat(enrollmentParameterReader.readParameter(null, true),
-                is(Config.ServiceInstanceParameters.Enrollment.standard));
-        assertThat(enrollmentParameterReader.readParameter(null, false),
-                is(nullValue()));
-
-        //bad syntax is well thrown
+    public void test_auto_enrollment_fails_to_read_bad_syntax() {
+        //Given the parameter reader for auto enrollment
+        ParameterReader<Enrollment> enrollmentParameterReader = factory.buildAutoEnrollmentReader();
+        //When we submit a bad syntax
+        //Then it fails
         verifyThrown(() -> enrollmentParameterReader
                         .readParameter("fart", true),
                 InvalidParameterException.class,
                 parameterChecked ->
                         assertThat(parameterChecked.getParameterName(),
-                                is(equalTo(Config.ServiceInstanceParameters.AUTO_ENROLLMENT))));
-
-        //good syntax
-        Config.ServiceInstanceParameters.Enrollment enrollment = enrollmentParameterReader
-                .readParameter(Config.ServiceInstanceParameters.Enrollment.standard.name(), true);
-        assertThat(enrollment, is(equalTo(Config
-                .ServiceInstanceParameters.Enrollment.standard)));
-        enrollment = enrollmentParameterReader
-                .readParameter(Config.ServiceInstanceParameters.Enrollment.forced.name(), false);
-        assertThat(enrollment, is(equalTo(Config
-                .ServiceInstanceParameters.Enrollment.forced)));
+                                is(equalTo(ServiceInstanceParameters.AUTO_ENROLLMENT))));
     }
 
     @Test
-    public void testBuildExcludeFromAutoEnrollmentReader() {
+    public void test_auto_enrollment_handle_right_parameter() {
+        //Given the parameter reader for auto enrollment
+        ParameterReader<Enrollment> enrollmentParameterReader = factory.buildAutoEnrollmentReader();
+        //When we ask the parameter
+        String parameter = enrollmentParameterReader.getParameterName();
+        //Then we obtain the auto enrollment parameter
+        assertThat(parameter, is(equalTo(ServiceInstanceParameters.AUTO_ENROLLMENT)));
+    }
+
+    @Test
+    public void test_auto_enrollment_read_parameter() {
+        //Given the parameter reader for auto enrollment
+        ParameterReader<Enrollment> enrollmentParameterReader = factory.buildAutoEnrollmentReader();
+        //When we read a good syntax
+        Enrollment standard = enrollmentParameterReader
+                .readParameter(Enrollment.standard.name(), true);
+        Enrollment forced = enrollmentParameterReader
+                .readParameter(Enrollment.forced.name(), false);
+        //Then we obtained the values from enum
+        assertThat(standard, is(equalTo(Enrollment.standard)));
+        assertThat(forced, is(equalTo(Enrollment.forced)));
+
+    }
+
+    @Test
+    public void test_auto_enrollment_returns_default_when_null_submitted_with_default() {
+        //Given the parameter reader for auto enrollment
+        ParameterReader<Enrollment> enrollmentParameterReader = factory.buildAutoEnrollmentReader();
+        //When we ask to read null with default
+        Enrollment withDefault = enrollmentParameterReader.readParameter(null, true);
+        //Then it returns the value from config
+        assertThat(withDefault, is(equalTo(Enrollment.standard)));
+    }
+
+    @Test
+    public void test_auto_enrollment_returns_null_when_null_submitted_without_default() {
+        //Given the parameter reader for auto enrollment
+        ParameterReader<Enrollment> enrollmentParameterReader = factory.buildAutoEnrollmentReader();
+        //When we ask to read null with default
+        Enrollment withoutDefault = enrollmentParameterReader.readParameter(null, false);
+        //Then it returns null
+        assertThat(withoutDefault, is(nullValue()));
+    }
+
+    @Test
+    public void test_exclude_fails_to_read_bad_syntax() {
+        //Given the parameter reader for exclude from auto enrollment
         ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
                 .buildExcludeFromAutoEnrollmentReader();
-        assertThat(excludeFromAutoEnrollmentReader.getParameterName(),
-                is(equalTo(Config.ServiceInstanceParameters.EXCLUDE_FROM_AUTO_ENROLLMENT)));
-
-        //default case are null
-        assertThat(excludeFromAutoEnrollmentReader.readParameter(null, false),
-                is(nullValue()));
-        assertThat(excludeFromAutoEnrollmentReader.readParameter("", false),
-                is(nullValue()));
-        assertThat(excludeFromAutoEnrollmentReader.readParameter(null, false),
-                is(nullValue()));
-
-        //bad syntax is well thrown
+        //When we submit a bad syntax
+        //Then it fails
         verifyThrown(() -> excludeFromAutoEnrollmentReader
                         .readParameter("*",
                                 true),
                 InvalidParameterException.class,
                 parameterChecked ->
                         assertThat(parameterChecked.getParameterName(),
-                                is(equalTo(Config.ServiceInstanceParameters.EXCLUDE_FROM_AUTO_ENROLLMENT))));
+                                is(equalTo(ServiceInstanceParameters.EXCLUDE_FROM_AUTO_ENROLLMENT))));
+    }
 
+    @Test
+    public void test_exclude_handle_right_parameter() {
+        //Given the parameter reader for exclude from auto enrollment
+        ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
+                .buildExcludeFromAutoEnrollmentReader();
+        //When we ask the parameter
+        String parameter = excludeFromAutoEnrollmentReader.getParameterName();
+        //Then we obtain the exclude from auto enrollment parameter
+        assertThat(parameter, is(equalTo(ServiceInstanceParameters.EXCLUDE_FROM_AUTO_ENROLLMENT)));
+    }
+
+    @Test
+    public void test_exclude_read_parameter() {
+        //Given the parameter reader for exclude from auto enrollment
+        ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
+                .buildExcludeFromAutoEnrollmentReader();
+        //When we read a good syntax
         String pattern = ".*";
         Pattern result = excludeFromAutoEnrollmentReader.readParameter(pattern, true);
+        //Then we obtained the right compiled pattern
         assertThat(result, is(not(nullValue())));
         assertThat(result.pattern(), is(equalTo(pattern)));
     }
 
     @Test
-    public void testBuildSecretReader() {
-        ParameterReader<String> secretReader = factory.buildSecretReader();
-        assertThat(secretReader.getParameterName(), is(equalTo(Config.ServiceInstanceParameters.SECRET)));
-
-        //default case are null
-        assertThat(secretReader.readParameter(null, false), is(nullValue()));
-        assertThat(secretReader.readParameter(null, false), is(nullValue()));
-
-        String secret = "P@ssword!";
-        assertThat(secretReader.readParameter(secret, true), is(equalTo(secret)));
+    public void test_exclude_returns_null_when_null_submitted_with_default() {
+        //Given the parameter reader for exclude from auto enrollment
+        ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
+                .buildExcludeFromAutoEnrollmentReader();
+        //When we ask to read null with default
+        Pattern withDefault = excludeFromAutoEnrollmentReader.readParameter(null, true);
+        //Then it returns null
+        assertThat(withDefault, is(nullValue()));
     }
+
+    @Test
+    public void test_exclude_returns_null_when_null_submitted_without_default() {
+        //Given the parameter reader for exclude from auto enrollment
+        ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
+                .buildExcludeFromAutoEnrollmentReader();
+        //When we ask to read null with default
+        Pattern withoutDefault = excludeFromAutoEnrollmentReader.readParameter(null, true);
+        //Then it returns null
+        assertThat(withoutDefault, is(nullValue()));
+    }
+
+    @Test
+    public void test_exclude_trim_submitted_values() {
+        //Given the parameter reader for exclude from auto enrollment
+        ParameterReader<Pattern> excludeFromAutoEnrollmentReader = factory
+                .buildExcludeFromAutoEnrollmentReader();
+        //When we ask to read a value full of spaces with default
+        Pattern withSpaces = excludeFromAutoEnrollmentReader.readParameter("     ", true);
+        //Then it returns null
+        assertThat(withSpaces, is(nullValue()));
+    }
+
+    @Test
+    public void test_idle_duration_fails_to_read_bad_syntax() {
+        //Given the parameter reader for idle duration
+        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
+        //When we submit a bad syntax
+        //Then it fails
+        verifyThrown(() -> idleDurationReader
+                        .readParameter("12",
+                                true),
+                InvalidParameterException.class,
+                parameterChecked ->
+                        assertThat(parameterChecked.getParameterName(),
+                                is(equalTo(ServiceInstanceParameters.IDLE_DURATION))));
+    }
+
+    @Test
+    public void test_idle_duration_handle_right_parameter() {
+        //Given the parameter reader for idle duration
+        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
+        //When we ask the parameter
+        String parameter = idleDurationReader.getParameterName();
+        //Then we obtain the duration parameter
+        assertThat(parameter, is(equalTo(ServiceInstanceParameters.IDLE_DURATION)));
+    }
+
+    @Test
+    public void test_idle_duration_read_parameter() {
+        //Given the parameter reader for idle duration
+        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
+        //When we read a good parameter
+        Duration duration = idleDurationReader.readParameter("PT12M", true);
+        //Then the value returned is ok
+        assertThat(duration, is(equalTo(Duration.ofMinutes(12))));
+    }
+
+    @Test
+    public void test_idle_duration_returns_default_when_null_submitted_with_default() {
+        //Given the parameter reader for idle duration
+        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
+        //When we ask to read null with default
+        Duration withDefault = idleDurationReader.readParameter(null, true);
+        //Then it returns the value from config
+        assertThat(withDefault, is(equalTo(Config.DEFAULT_INACTIVITY_PERIOD)));
+    }
+
+    @Test
+    public void test_idle_duration_returns_null_when_null_submitted_without_default() {
+        //Given the parameter reader for idle duration
+        ParameterReader<Duration> idleDurationReader = factory.buildIdleDurationReader();
+        //When we ask to read null without  default
+        Duration withoutDefault = idleDurationReader.readParameter(null, false);
+        //Then it returns null
+        assertThat(withoutDefault, is(nullValue()));
+    }
+
+    @Test
+    public void test_secret_handle_right_parameter() {
+        //Given the parameter reader for secret
+        ParameterReader<String> secretReader = factory.buildSecretReader();
+        //When we ask the parameter
+        String parameter = secretReader.getParameterName();
+        //Then we obtain the auto enrollment parameter
+        assertThat(parameter, is(equalTo(ServiceInstanceParameters.SECRET)));
+    }
+
+    @Test
+    public void test_secret_read_parameter() {
+        //Given the parameter reader for secret
+        ParameterReader<String> secretReader = factory.buildSecretReader();
+        //When we read a good syntax
+        String secret = "P@ssword!";
+        String readSecret = secretReader.readParameter(secret, true);
+        //Then we obtained the value submitted
+        assertThat(readSecret, is(equalTo(secret)));
+    }
+
+    @Test
+    public void test_secret_returns_null_when_null_submitted_with_default() {
+        //Given the parameter reader for secret
+        ParameterReader<String> secretReader = factory.buildSecretReader();
+        //When we ask to read null with default
+        String withDefault = secretReader.readParameter(null, true);
+        //Then it returns null
+        assertThat(withDefault, is(nullValue()));
+    }
+
+    @Test
+    public void test_secret_returns_null_when_null_submitted_without_default() {
+        //Given the parameter reader for secret
+        ParameterReader<String> secretReader = factory.buildSecretReader();
+        //When we ask to read null with default
+        String withoutDefault = secretReader.readParameter(null, true);
+        //Then it returns null
+        assertThat(withoutDefault, is(nullValue()));
+    }
+
 }
