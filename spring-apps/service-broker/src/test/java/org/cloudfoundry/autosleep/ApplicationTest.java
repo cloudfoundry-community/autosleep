d8c6735d8db6cb93ff58e4b3c3b3345f5b96d422
Arnaud Ruffin <arnaud.ruffin@orange.com>
2016-05-04 17:14:27 +0200

    first step toward #185. One app, but splitted between a common and a app module from gradle point of view

---
+++ spring-apps/service-broker/src/test/java/org/cloudfoundry/autosleep/ApplicationTest.java
@@ -0,0 +1,71 @@
+/*
+ * Autosleep
+ * Copyright (C) 2016 Orange
+ * Authors: Benjamin Einaudi   benjamin.einaudi@orange.com
+ *          Arnaud Ruffin      arnaud.ruffin@orange.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.autosleep;
+
+import javassist.NotFoundException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.cloudfoundry.autosleep.config.Config;
+import org.cloudfoundry.autosleep.util.BeanGenerator;
+import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.logging.LoggingClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.annotation.PostConstruct;
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationTest.MockClientConfiguration.class, Application.class})
+@WebAppConfiguration
+public class ApplicationTest {
+
+    @Configuration
+    @Slf4j
+    public static class MockClientConfiguration {
+
+        @Getter(onMethod = @__(@Bean))
+        private CloudFoundryClient cloudFoundryClient;
+
+        @Getter(onMethod = @__(@Bean))
+        private LoggingClient logClient;
+
+        @PostConstruct
+        public void initClientEnvironment() throws NotFoundException {
+            cloudFoundryClient = mock(CloudFoundryClient.class);
+            logClient = mock(LoggingClient.class);
+            System.setProperty(Config.EnvKey.APPLICATION_DESCRIPTION_ENVIRONMENT_KEY,
+                    BeanGenerator.getSampleVcapApplication(UUID.randomUUID(), "autosleep",
+                            "http://somewhere-else.org"));
+        }
+
+    }
+
+    @Test
+    public void testDummy() {
+    }
+}
---
+++ spring-apps/service-broker/src/test/java/org/cloudfoundry/autosleep/util/BeanGenerator.java
@@ -0,0 +1,193 @@
+/*
+ * Autosleep
+ * Copyright (C) 2016 Orange
+ * Authors: Benjamin Einaudi   benjamin.einaudi@orange.com
+ *          Arnaud Ruffin      arnaud.ruffin@orange.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.autosleep.util;
+
+import org.cloudfoundry.autosleep.access.cloudfoundry.model.ApplicationIdentity;
+import org.cloudfoundry.autosleep.access.dao.model.ApplicationInfo;
+import org.cloudfoundry.autosleep.access.dao.model.Binding;
+import org.cloudfoundry.autosleep.access.dao.model.Binding.ResourceType;
+import org.cloudfoundry.autosleep.access.dao.model.SpaceEnrollerConfig;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.cloudfoundry.autosleep.access.dao.model.Binding.ResourceType.Route;
+
+public class BeanGenerator {
+
+    public static final UUID ORG_TEST = UUID.randomUUID();
+
+    public static final UUID PLAN_ID = UUID.randomUUID();
+
+    public static final UUID SERVICE_DEFINITION_ID = UUID.randomUUID();
+
+    public static final UUID SPACE_TEST = UUID.randomUUID();
+
+    public static ApplicationIdentity createAppIdentity(String appUuid) {
+        return ApplicationIdentity.builder()
+                .guid(appUuid)
+                .name(appUuid + "-name")
+                .build();
+    }
+
+    public static ApplicationInfo createAppInfo() {
+        return createAppInfoLinkedToService(null, null);
+    }
+
+    public static ApplicationInfo createAppInfoLinkedToService(String serviceId) {
+        return createAppInfoLinkedToService(null, serviceId);
+    }
+
+    public static ApplicationInfo createAppInfoLinkedToService(String appUuid, String serviceId) {
+        if (appUuid == null) {
+            appUuid = UUID.randomUUID().toString();
+        }
+        ApplicationInfo applicationInfo = ApplicationInfo.builder()
+                .uuid(appUuid)
+                .build();
+        if (serviceId != null) {
+            applicationInfo.getEnrollmentState().addEnrollmentState(serviceId);
+        }
+        return applicationInfo;
+    }
+
+    public static ApplicationInfo createAppInfoWithDiagnostic(String appUuid, String name, String state) {
+        return createAppInfoWithDiagnostic(appUuid, name, state, null, null);
+    }
+
+    public static ApplicationInfo createAppInfoWithDiagnostic(String appUuid, String name, String state, Instant
+            lastLogDate, Instant lastEventDate) {
+        ApplicationInfo applicationInfo = ApplicationInfo.builder()
+                .uuid(appUuid)
+                .build();
+        applicationInfo.updateDiagnosticInfo(createAppLog(lastLogDate),
+                createCloudEvent(lastEventDate),
+                name,
+                state);
+        return applicationInfo;
+    }
+
+    public static ApplicationInfo.DiagnosticInfo.ApplicationLog createAppLog(Instant instant) {
+        return ApplicationInfo.DiagnosticInfo.ApplicationLog.builder()
+                .message("fakelog")
+                .timestamp((instant != null ? instant : Instant.now()).toEpochMilli())
+                .messageType("STDOUT")
+                .sourceName("sourceName")
+                .sourceId("sourceId")
+                .build();
+    }
+
+    public static ApplicationInfo.DiagnosticInfo.ApplicationLog createAppLog() {
+        return createAppLog(null);
+    }
+
+    public static Binding createBinding(String serviceId, String bindingId, String appId) {
+        if (serviceId == null) {
+            serviceId = UUID.randomUUID().toString();
+        }
+        if (bindingId == null) {
+            bindingId = UUID.randomUUID().toString();
+        }
+        if (appId == null) {
+            appId = UUID.randomUUID().toString();
+        }
+        return Binding.builder()
+                .serviceBindingId(bindingId)
+                .serviceInstanceId(serviceId)
+                .resourceId(appId)
+                .resourceType(ResourceType.Application)
+                .build();
+    }
+
+    public static Binding createBinding() {
+        return createBinding(null, null, null);
+    }
+
+    public static ApplicationInfo.DiagnosticInfo.ApplicationEvent createCloudEvent(Instant instant) {
+        ApplicationInfo.DiagnosticInfo.ApplicationEvent event =
+                ApplicationInfo.DiagnosticInfo.ApplicationEvent.builder()
+                        .name("fakeEvent")
+                        .timestamp((instant != null ? instant : Instant.now()).toEpochMilli())
+                        .build();
+        return event;
+    }
+
+    public static ApplicationInfo.DiagnosticInfo.ApplicationEvent createCloudEvent() {
+        return createCloudEvent(null);
+    }
+
+    public static Binding createRouteBinding(String bindingId,
+                                             String serviceId, String routeId) {
+        return Binding.builder()
+                .resourceType(Route)
+                .serviceBindingId(bindingId)
+                .resourceId(routeId)
+                .serviceInstanceId(serviceId).build();
+    }
+
+    public static Binding createRouteBinding(String bindingId) {
+        return createRouteBinding(bindingId, "", "arouteId");
+    }
+
+    public static SpaceEnrollerConfig createServiceInstance() {
+        return createServiceInstance(UUID.randomUUID().toString());
+    }
+
+    public static SpaceEnrollerConfig createServiceInstance(String serviceId) {
+        return SpaceEnrollerConfig.builder()
+                .serviceDefinitionId(SERVICE_DEFINITION_ID.toString())
+                .planId(PLAN_ID.toString())
+                .organizationId(ORG_TEST.toString())
+                .spaceId(SPACE_TEST.toString())
+                .id(serviceId).build();
+    }
+
+    public static String getSampleVcapApplication(UUID applicationId, String applicationName, String... uris) {
+        return "{\"limits\":{\"mem\":1024,\"disk\":1024,\"fds\":16384},"
+                + "\"application_id\":\"" + applicationId.toString() + "\","
+                + "\"application_version\":\"b546c9d4-8885-4d50-a855-490ddb5b5a1c\","
+                + "\"application_name\":\"" + applicationName + "\","
+                + "\"application_uris\":["
+                + String.join(", ",
+                Arrays.asList(uris).stream()
+                        .map(uri -> "\"" + uri + "\"")
+                        .collect(Collectors.toList()))
+                + "],"
+                + " \"version\":\"b546c9d4-8885-4d50-a855-490ddb5b5a1c\","
+                + "\"name\":\"autosleep-app\","
+                + "\"space_name\":\"autosleep\""
+                + ",\"space_id\":\"2d745a4b-67e3-4398-986e-2adbcf8f7ec9\","
+                + "\"uris\":[\"autosleep-app-ben.cf.ns.nd-paas.itn.ftgroup\","
+                + "\"autosleep-nonnational-artotype.cf.ns.nd-paas.itn.ftgroup\","
+                + "\"autosleep.cf.ns.nd-paas.itn.ftgroup\"]"
+                + ",\"users\":null,"
+                + "\"instance_id\":\"7984a682cab9447891674f862299c77f\","
+                + "\"instance_index\":0,"
+                + "\"host\":\"0.0.0.0\","
+                + "\"port\":61302,"
+                + "\"started_at\":\"2015-11-18 15:49:06 +0000\","
+                + "\"started_at_timestamp\":1447861746,"
+                + "\"start\":\"2015-11-18 15:49:06 +0000\","
+                + "\"state_timestamp\":1447861746"
+                + "}";
+    }
+}
