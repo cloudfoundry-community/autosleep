9249cd229649e46a60a82bb0662d96f7c82bd2d5
Joao Viegas <joao.viegas@mindprogeny.com>
2018-06-19 11:15:33 +0200

    cleanup unused import and bump spring dependency

--- spring-apps/autosleep-core/src/main/java/org/cloudfoundry/autosleep/ui/servicebroker/service/AutosleepBindingService.java
+++ spring-apps/autosleep-core/src/main/java/org/cloudfoundry/autosleep/ui/servicebroker/service/AutosleepBindingService.java
@@ -45,7 +45,6 @@ import org.springframework.cloud.servicebroker.service.ServiceInstanceBindingSer
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.cloudfoundry.autosleep.access.dao.model.Binding.ResourceType.Application;
